88d9b5c046204bd724cc476db11fad8c85dd8c20
Elad <elad@dekel.ca>
2021-02-19 21:35:47 -0500

    Initial Commit

---
+++ CameraShy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraShy
+//
+//  Created by Elad Dekel on 2021-02-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
