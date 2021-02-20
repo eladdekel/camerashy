0fb4370cbdcdd6f48168c511c037467d6ebd4832
Elad Dekel <eladdekel2@gmail.com>
2021-02-20 14:16:18 -0500

    All Together!

--- CameraShy/Connector.swift
+++ CameraShy/Connector.swift
@@ -8,16 +8,16 @@ struct VCSwiftUIView: UIViewControllerRepresentable {
     let storyboard: String = "Main"
     let VC: String = "GameSetup"
 
-  func makeUIViewController(context: UIViewControllerRepresentableContext<VCSwiftUIView>) -> ViewController {
+  func makeUIViewController(context: UIViewControllerRepresentableContext<VCSwiftUIView>) -> UINavigationController {
     
     //Load the storyboard
     let loadedStoryboard = UIStoryboard(name: storyboard, bundle: nil)
     
     //Load the ViewController
-     return loadedStoryboard.instantiateViewController(withIdentifier: VC) as! ViewController
+     return loadedStoryboard.instantiateViewController(withIdentifier: VC) as! UINavigationController
     
   }
   
-  func updateUIViewController(_ uiViewController: ViewController, context: UIViewControllerRepresentableContext<VCSwiftUIView>) {
+  func updateUIViewController(_ uiViewController: UINavigationController, context: UIViewControllerRepresentableContext<VCSwiftUIView>) {
   }
 }
